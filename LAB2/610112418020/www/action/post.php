7ad916179f691f85ff644c032e804946f008965d
pipat000 <pbtonnnpipat@gmail.com>
2020-03-10 11:53:51 +0700

    first commit

---
+++ LAB2/610112418020/www/action/post.php
@@ -0,0 +1,20 @@
+<?php
+
+$sport = $_POST['sl_sport']?$_POST['sl_sport']:$_GET['getSport'];
+cal_sport($sport);
+
+function cal_sport($sport){
+    switch ($sport){
+        case "1":
+            $sport = "Fottball";
+            break;
+        case "2":
+            $sport = "Tenis";
+            break;
+        default:
+            $sport = "Out of type.";
+    }  echo $sport;
+        };
+
+
+?>
